f40262361baf632d3153dae3737ea2d4e0d0f96a
usfaql <usfaql@gmail.com>
2024-01-12 02:22:51 +0300

    get Data User to Edit Profile without save edit

--- frontend/src/components/EditProfile/EditProfile.js
+++ frontend/src/components/EditProfile/EditProfile.js
@@ -9,24 +9,60 @@ function EditProfile() {
     const config = {
       headers: { Authorization: `Bearer ${token}` }
     };
-
-    axios.get(`http://localhost:5000/users/${userId}`, config).then((result) => {
-      console.log(result);
-    }).catch((err) => {
-        
-    });
+    useEffect(()=>{
+      axios.get(`http://localhost:5000/users/${userId}`, config).then((result) => {
+        setDataUser(result.data.user);
+      }).catch((err) => {
+          
+      });
+    },[]);
+   
   return (
-    <div style={{display:"flex",height:"85vh", flexDirection:"row", justifyContent:"center", placeContent:"center"}}>
+    <div className='main-edit'>
       
       <div className='contener-edit-profile'>
-        <div>Image User</div>
-        <div>
-        <input defaultValue={"firstName"}/>
-          <input defaultValue={"lastName"}/>
+        <div className='contener-info-edit-main'>
+          <div className='contener-info-edit'>
+
+          
+          <div className='img-name-info'>          
+            <img className='image-profile' src={`${dataUser.image}`}/>
+            <div>{dataUser.firstName +" "+ dataUser.lastName}</div>
+          </div>
+          <div>
+            <button className='btn-open-profile'>Save Change</button>
+          </div>
+          </div>
+        </div>
+        <div style={{display:"flex", flexDirection:"column",placeItems:"center"}}>
+          <div style={{textAlign:"start", marginLeft:"20px", color:"rgb(160,160,160)",fontSize:"18px", width:"50%"}}>Personal details</div>
+          <div className='line' style={{maxWidth:"50%"}}></div>
+        </div>
+        
+        <div className='contenter-input'>
+          <div className='label-and-input'>
+          <div>
+            <div className='label'>First Name:</div>
+            <input id='first-name' className='input-edit' defaultValue={dataUser.firstName}/>
+          </div>
+          <div>
+            <div className='label'>Last Name:</div>
+            <input id='last-name' className='input-edit' defaultValue={dataUser.lastName}/>
+          </div>
+          
         </div>
 
-        <div><input defaultValue={"Email"}/></div>
-        <div><input defaultValue={"Phone Number"}/></div>
+        <div className='label-and-input'>
+        <div>
+          <div className='label'>Email:</div>
+          <input id='email' className='input-edit' defaultValue={dataUser.email}/>
+        </div>
+        <div>
+          <div className='label'>Phone:</div>
+          <input id='phone' className='input-edit' defaultValue={dataUser.phoneNumber}/>
+          </div>
+        </div>
+        </div>
 
       </div>
     </div>
